522f677d99662630e55793c0cebee0b42fb1ba63
Reza Abid Abistha <reza.abid.abistha-2019@fst.unair.ac.id>
2023-06-30 19:03:29 +0700

    Final Store Procedure

--- SP_summary_order_status.sql
+++ SP_summary_order_status.sql
@@ -4,13 +4,13 @@ CREATE PROCEDURE dbo.summary_order_status
 )
 AS
 BEGIN
-	SELECT a.OrderID, b.CustomerName, c.ProductName, a.Quantity, d.StatusOrder
-	FROM FactSalesOrder a
-	INNER JOIN DimCustomer b ON a.CustomerID = b.CustomerID
-	INNER JOIN DimProduct c ON a.ProductID = c.ProductID
-	INNER JOIN DimStatusOrder d ON a.StatusID = d.StatusID
-	WHERE d.StatusID = @StatusID;
+	SELECT FSO.OrderID, DC.CustomerName, DP.ProductName, FSO.Quantity, DSO.StatusOrder
+	FROM FactSalesOrder FSO
+	INNER JOIN DimCustomer DC ON FSO.CustomerID = DC.CustomerID
+	INNER JOIN DimProduct DP ON FSO.ProductID = DP.ProductID
+	INNER JOIN DimStatusOrder DSO ON FSO.StatusID = DSO.StatusID
+	WHERE DSO.StatusID = @StatusID;
 END;
 
-EXEC dbo.summary_order_status @StatusID = 2;
+EXEC dbo.summary_order_status @StatusID = 1;
 
